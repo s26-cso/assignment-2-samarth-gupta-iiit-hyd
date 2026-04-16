d5ffe5d9287ce05eec43b4163ef58cb9f0e9c231
samarth-gupta-iiit-hyd <kg086226@gmail.com>
2026-04-16 23:56:10 +0530

    Update q2.s
    Refactor string_to_int function and remove unused stack operations. Update main function to improve clarity and efficiency.

--- q2/q2.s
+++ q2/q2.s
@@ -3,76 +3,34 @@ fmt: .string "%d "
 fmt_last: .string "%d"
 newline: .string "\n"
 
-.section .data
-.balign 8
-head: .dword 0
-
 .section .text
 .globl main
 .extern printf
 .extern malloc
 
 string_to_int:
-    add  t0, x0, x0
-    add  t3, x0, x0
+    li   t0, 0
+    li   t3, 0
     lb   t1, 0(a0)
-    addi t2, x0, 45
-    bne  t1, t2, stoi_loop
-    addi t3, x0, 1
+    li   t2, 45
+    bne  t1, t2, stoi_loop_start
+    li   t3, 1
     addi a0, a0, 1
-stoi_loop:
+stoi_loop_start:
     lb   t1, 0(a0)
-    beq  t1, x0, stoi_end
+    beqz t1, stoi_done
     addi t1, t1, -48
-    addi t2, x0, 10
+    li   t2, 10
     mul  t0, t0, t2
     add  t0, t0, t1
     addi a0, a0, 1
-    jal  x0, stoi_loop
-stoi_end:
-    beq  t3, x0, stoi_ret
-    sub  t0, x0, t0
+    j    stoi_loop_start
+stoi_done:
+    beqz t3, stoi_ret
+    neg  t0, t0
 stoi_ret:
-    add  a0, t0, x0
-    jalr x0, ra, 0
-
-push:
-    addi sp, sp, -16
-    sd   ra, 8(sp)
-    sd   s0, 0(sp)
-    add  s0, a0, x0      
-    addi a0, x0, 16      
-    jal  ra, malloc
-    add  t0, a0, x0
-    sw   s0, 0(t0)       
-1:  auipc t1, %pcrel_hi(head)
-    ld   t2, %pcrel_lo(1b)(t1)
-    sd   t2, 8(t0)
-    sd   t0, %pcrel_lo(1b)(t1)
-    ld   ra, 8(sp)
-    ld   s0, 0(sp)
-    addi sp, sp, 16
-    jalr x0, ra, 0
-
-pop:
-1:  auipc t0, %pcrel_hi(head)
-    addi t0, t0, %pcrel_lo(1b)
-    ld   t1, 0(t0)
-    beq  t1, x0, pop_done 
-    ld   t2, 8(t1)
-    sd   t2, 0(t0)
-pop_done:
-    jalr x0, ra, 0
-
-top:
-1:  auipc t0, %pcrel_hi(head)
-    addi t0, t0, %pcrel_lo(1b)
-    ld   t1, 0(t0)
-    addi a0, x0, -1
-    beq  t1, x0, top_done
-    lw   a0, 0(t1)
-top_done:
-    jalr x0, ra, 0
+    mv   a0, t0
+    ret
 
 main:
     addi sp, sp, -80
@@ -83,106 +41,91 @@ main:
     sd   s3, 40(sp)
     sd   s4, 32(sp)
     sd   s5, 24(sp)
+    sd   s6, 16(sp)
 
     addi s0, a0, -1
-    add  s3, a1, x0
-    beq  s0, x0, exit_fast
+    mv   s6, a1
+    blez s0, exit_main
 
     slli a0, s0, 2
-    jal  ra, malloc
-    add  s1, a0, x0
+    call malloc
+    mv   s1, a0
     slli a0, s0, 2
-    jal  ra, malloc
-    add  s2, a0, x0
+    call malloc
+    mv   s2, a0
+    slli a0, s0, 2
+    call malloc
+    mv   s3, a0
+    li   s4, -1
 
-1:  auipc t0, %pcrel_hi(head)
-    sd   x0, %pcrel_lo(1b)(t0)
-
-    addi s4, x0, 1
-argtoint:
-    blt  s0, s4, nge
-    slli t1, s4, 3
-    add  t1, s3, t1
-    ld   a0, 0(t1)
-    jal  ra, string_to_int
-    addi t1, s4, -1
-    slli t1, t1, 2
+    li   s5, 0
+parse_args:
+    bge  s5, s0, nge_init
+    addi t0, s5, 1
+    slli t0, t0, 3
+    add  t0, s6, t0
+    ld   a0, 0(t0)
+    call string_to_int
+    slli t1, s5, 2
     add  t1, s1, t1
     sw   a0, 0(t1)
-    addi s4, s4, 1
-    jal  x0, argtoint
+    addi s5, s5, 1
+    j    parse_args
 
-nge:
-    addi s4, s0, -1
-loop_main:
-    blt  s4, x0, print
-loop_while:
-1:  auipc t0, %pcrel_hi(head)
-    ld   t1, %pcrel_lo(1b)(t0)
-    beq  t1, x0, noelem      
-
-    jal  ra, top
-    add  s5, a0, x0          
-    
-    slli t1, s5, 2
-    add  t1, s1, t1
-    lw   t1, 0(t1)           # t1 = arr[stack.top()]
-
-    slli t2, s4, 2
-    add  t2, s1, t2
-    lw   t2, 0(t2)           # t2 = arr[i]
-
-    bgt  t1, t2, found       # if arr[stack.top()] > arr[i]
-    jal  ra, pop
-    jal  x0, loop_while
-
-noelem:
+nge_init:
+    addi s5, s0, -1
+nge_loop:
+    bltz s5, print_init
+    slli t0, s5, 2
+    add  t0, s1, t0
+    lw   t0, 0(t0)
+stack_while:
+    bltz s4, stack_is_empty
     slli t1, s4, 2
-    add  t1, s2, t1
-    addi t2, x0, -1
-    sw   t2, 0(t1)
-    jal  x0, push_idx
-
-found:
-    slli t1, s5, 2
-    add  t1, s1, t1
-    lw   t1, 0(t1)           # Get VALUE of NGE
-    slli t2, s4, 2
-    add  t2, s2, t2
-    sw   t1, 0(t2)           
-
-push_idx:
-    add  a0, s4, x0          
-    jal  ra, push
+    add  t1, s3, t1
+    lw   t1, 0(t1)
+    bgt  t1, t0, nge_found
     addi s4, s4, -1
-    jal  x0, loop_main
+    j    stack_while
+nge_found:
+    slli t2, s5, 2
+    add  t2, s2, t2
+    sw   t1, 0(t2)
+    j    push_val
+stack_is_empty:
+    slli t2, s5, 2
+    add  t2, s2, t2
+    li   t1, -1
+    sw   t1, 0(t2)
+push_val:
+    addi s4, s4, 1
+    slli t2, s4, 2
+    add  t2, s3, t2
+    sw   t0, 0(t2)
+    addi s5, s5, -1
+    j    nge_loop
 
-print:
-    add  s4, x0, x0
+print_init:
+    li   s5, 0
 print_loop:
-    bge  s4, s0, exit_newline
-    slli t0, s4, 2
+    bge  s5, s0, print_done
+    slli t0, s5, 2
     add  t0, s2, t0
     lw   a1, 0(t0)
     addi t1, s0, -1
-    beq  s4, t1, last_val
-1:  auipc a0, %pcrel_hi(fmt)
-    addi a0, a0, %pcrel_lo(1b)
-    jal  ra, printf
-    addi s4, s4, 1
-    jal  x0, print_loop
+    beq  s5, t1, print_last
+    la   a0, fmt
+    call printf
+    addi s5, s5, 1
+    j    print_loop
+print_last:
+    la   a0, fmt_last
+    call printf
+print_done:
+    la   a0, newline
+    call printf
 
-last_val:
-1:  auipc a0, %pcrel_hi(fmt_last)
-    addi a0, a0, %pcrel_lo(1b)
-    jal  ra, printf
-
-exit_newline:
-1:  auipc a0, %pcrel_hi(newline)
-    addi a0, a0, %pcrel_lo(1b)
-    jal  ra, printf
-
-exit_fast:
+exit_main:
     ld   ra, 72(sp)
     ld   s0, 64(sp)
     ld   s1, 56(sp)
@@ -190,6 +133,7 @@ exit_fast:
     ld   s3, 40(sp)
     ld   s4, 32(sp)
     ld   s5, 24(sp)
+    ld   s6, 16(sp)
     addi sp, sp, 80
-    add  a0, x0, x0
-    jalr x0, ra, 0
+    li   a0, 0
+    ret

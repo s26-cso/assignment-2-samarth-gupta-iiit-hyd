f6b16e6ca03e1eb9424aeebcd7c965c552964bc4
samarth-gupta-iiit-hyd <kg086226@gmail.com>
2026-04-17 19:27:49 +0530

    Update q2.s

--- q2/q2.s
+++ q2/q2.s
@@ -1,151 +1,198 @@
 .section .rodata
-fmt_ld:      .string "%ld"
-fmt_space:   .string " "
-fmt_newline: .string "\n"
+fmt:      .string "%ld"
+fmt_last: .string "%ld"
+fmt_space:  .string " "
+newline:    .string "\n"
 
-    .section .text
-    .global main
-    .type   main, @function
+.section .data
+.balign 8
+head: .dword 0
+
+.section .text
+.globl main
+.extern printf
+.extern malloc
+.extern free
+
+string_to_int:
+    add  t0, x0, x0
+    add  t3, x0, x0
+    lb   t1, 0(a0)
+    addi t2, x0, 45
+    bne  t1, t2, stoi_loop
+    addi t3, x0, 1
+    addi a0, a0, 1
+stoi_loop:
+    lb   t1, 0(a0)
+    beq  t1, x0, stoi_end
+    addi t1, t1, -48
+    addi t2, x0, 10
+    mul  t0, t0, t2
+    add  t0, t0, t1
+    addi a0, a0, 1
+    jal  x0, stoi_loop
+stoi_end:
+    beq  t3, x0, stoi_ret
+    sub  t0, x0, t0
+stoi_ret:
+    add  a0, t0, x0
+    jalr x0, ra, 0
+
+push:
+    add  t2, a0, x0
+    addi sp, sp, -16
+    sd   ra, 8(sp)
+    sd   t2, 0(sp)
+    addi a0, x0, 16
+    call malloc
+    add  t0, a0, x0
+    ld   ra, 8(sp)
+    ld   t2, 0(sp)
+    addi sp, sp, 16
+    sw   t2, 0(t0)
+1:  auipc t1, %pcrel_hi(head)
+    ld   t2, %pcrel_lo(1b)(t1)
+    sd   t2, 8(t0)
+    sd   t0, %pcrel_lo(1b)(t1)
+    jalr x0, ra, 0
+
+pop:
+1:  auipc t0, %pcrel_hi(head)
+    addi  t0, t0, %pcrel_lo(1b)
+    ld   t1, 0(t0)
+    ld   t2, 8(t1)
+    sd   t2, 0(t0)
+    jalr x0, ra, 0
+
+top:
+1:  auipc t0, %pcrel_hi(head)
+    addi  t0, t0, %pcrel_lo(1b)
+    ld   t1, 0(t0)
+    lw   a0, 0(t1)
+    jalr x0, ra, 0
 
 main:
-    addi    sp,  sp,  -80
-    sd      ra,  64(sp)
-    sd      s0,  56(sp)
-    sd      s1,  48(sp)
-    sd      s2,  40(sp)
-    sd      s3,  32(sp)
-    sd      s4,  24(sp)
-    sd      s5,  16(sp)
-    sd      s6,   8(sp)
-    sd      s7,   0(sp)
+    addi sp, sp, -64
+    sd   ra, 56(sp)
+    sd   s0, 48(sp)
+    sd   s1, 40(sp)
+    sd   s2, 32(sp)
+    sd   s3, 24(sp)
+    sd   s4, 16(sp)
 
-    addi    s0,  sp,  80
+    addi s0, a0, -1
+    add  s3, a1, x0
+    beq  s0, x0, exit
 
-    addi    s1,  a0,  -1
-    addi    s2,  a1,   8
+    slli a0, s0, 3
+    call malloc
+    add  s1, a0, x0
 
-    mv      s7,  s1
-    mv      s1,  s2
+    slli a0, s0, 3
+    call malloc
+    add  s2, a0, x0
 
-    slli    a0,  s7,   3
-    call    malloc
-    mv      s2,  a0
+1:  auipc t0, %pcrel_hi(head)
+    sd   x0, %pcrel_lo(1b)(t0)
 
-    slli    a0,  s7,   3
-    call    malloc
-    mv      s3,  a0
+    addi s4, x0, 1
+argtoint:
+    blt  s0, s4, nge
+    slli t1, s4, 3
+    add  t1, s3, t1
+    ld   a0, 0(t1)
+    jal  ra, string_to_int
+    addi t1, s4, -1
+    slli t1, t1, 3
+    add  t1, s1, t1
+    sd   a0, 0(t1)
+    addi s4, s4, 1
+    jal  x0, argtoint
 
-    slli    a0,  s7,   3
-    call    malloc
-    mv      s4,  a0
+nge:
+    addi s4, s0, -1
+loop_main:
+    blt  s4, x0, print
+loop_while:
+1:  auipc t0, %pcrel_hi(head)
+    addi  t0, t0, %pcrel_lo(1b)
+    ld   t1, 0(t0)
+    beq  t1, x0, noelem
+    jal  ra, top
+    slli t1, a0, 3
+    add  t1, s1, t1
+    ld   t1, 0(t1)
+    slli t2, s4, 3
+    add  t2, s1, t2
+    ld   t2, 0(t2)
+    blt  t2, t1, found
+    jal  ra, pop
+    jal  x0, loop_while
+noelem:
+    slli t1, s4, 3
+    add  t1, s2, t1
+    addi t2, x0, -1
+    sd   t2, 0(t1)
+    jal  x0, push_i
+found:
+    jal  ra, top
+    slli t1, s4, 3
+    add  t1, s2, t1
+    sd   a0, 0(t1)
+push_i:
+    add  a0, s4, x0
+    jal  ra, push
+    addi s4, s4, -1
+    jal  x0, loop_main
 
-    li      s6,   0
-.Lparse_loop:
-    bge     s6,  s7,  .Lparse_done
-    slli    t0,  s6,   3
-    add     t0,  s1,  t0
-    ld      a0,  0(t0)
-    call    atoi
-    slli    a0,  a0,  32
-    srai    a0,  a0,  32
-    slli    t0,  s6,   3
-    add     t0,  s2,  t0
-    sd      a0,  0(t0)
-    addi    s6,  s6,   1
-    j       .Lparse_loop
-.Lparse_done:
+print:
+    add  s4, x0, x0
+print_loop:
+    bge  s4, s0, exit_print
+    beq  s4, x0, no_space
+1:  auipc a0, %pcrel_hi(fmt_space)
+    addi  a0, a0, %pcrel_lo(1b)
+    jal  ra, printf
+no_space:
+    slli t0, s4, 3
+    add  t0, s2, t0
+    ld   a1, 0(t0)
+1:  auipc a0, %pcrel_hi(fmt)
+    addi  a0, a0, %pcrel_lo(1b)
+    jal  ra, printf
+    addi s4, s4, 1
+    jal  x0, print_loop
 
-    li      s6,   0
-.Linit_loop:
-    bge     s6,  s7,  .Linit_done
-    slli    t0,  s6,   3
-    add     t0,  s3,  t0
-    li      t1,  -1
-    sd      t1,  0(t0)
-    addi    s6,  s6,   1
-    j       .Linit_loop
-.Linit_done:
+exit_print:
+1:  auipc a0, %pcrel_hi(newline)
+    addi  a0, a0, %pcrel_lo(1b)
+    jal  ra, printf
 
-    li      s5,  -1
-    addi    s6,  s7,  -1
+    add  a0, s1, x0
+    jal  ra, free
+    add  a0, s2, x0
+    jal  ra, free
 
-.Lnge_loop:
-    bltz    s6,  .Lnge_done
+    ld   ra, 56(sp)
+    ld   s0, 48(sp)
+    ld   s1, 40(sp)
+    ld   s2, 32(sp)
+    ld   s3, 24(sp)
+    ld   s4, 16(sp)
+    addi sp, sp, 64
+    add  a0, x0, x0
+    jalr x0, ra, 0
 
-    slli    t0,  s6,   3
-    add     t0,  s2,  t0
-    ld      t4,  0(t0)
-
-.Lpop_loop:
-    bltz    s5,  .Lpop_done
-    slli    t0,  s5,   3
-    add     t0,  s4,  t0
-    ld      t1,  0(t0)
-    slli    t2,  t1,   3
-    add     t2,  s2,  t2
-    ld      t3,  0(t2)
-    bgt     t3,  t4,  .Lpop_done
-    addi    s5,  s5,  -1
-    j       .Lpop_loop
-.Lpop_done:
-
-    bltz    s5,  .Lpush
-    slli    t0,  s5,   3
-    add     t0,  s4,  t0
-    ld      t1,  0(t0)
-    slli    t2,  s6,   3
-    add     t2,  s3,  t2
-    sd      t1,  0(t2)
-
-.Lpush:
-    addi    s5,  s5,   1
-    slli    t0,  s5,   3
-    add     t0,  s4,  t0
-    sd      s6,  0(t0)
-
-    addi    s6,  s6,  -1
-    j       .Lnge_loop
-.Lnge_done:
-
-    li      s6,   0
-.Lprint_loop:
-    bge     s6,  s7,  .Lprint_done
-
-    beqz    s6,  .Lno_space
-    la      a0,  fmt_space
-    call    printf
-.Lno_space:
-    slli    t0,  s6,   3
-    add     t0,  s3,  t0
-    ld      a1,  0(t0)
-    la      a0,  fmt_ld
-    call    printf
-
-    addi    s6,  s6,   1
-    j       .Lprint_loop
-.Lprint_done:
-
-    la      a0,  fmt_newline
-    call    printf
-
-    mv      a0,  s2
-    call    free
-    mv      a0,  s3
-    call    free
-    mv      a0,  s4
-    call    free
-
-    ld      ra,  64(sp)
-    ld      s0,  56(sp)
-    ld      s1,  48(sp)
-    ld      s2,  40(sp)
-    ld      s3,  32(sp)
-    ld      s4,  24(sp)
-    ld      s5,  16(sp)
-    ld      s6,   8(sp)
-    ld      s7,   0(sp)
-    addi    sp,  sp,   80
-    li      a0,  0
-    ret
-
-    .size   main, .-main
+exit:
+1:  auipc a0, %pcrel_hi(newline)
+    addi  a0, a0, %pcrel_lo(1b)
+    jal  ra, printf
+    ld   ra, 56(sp)
+    ld   s0, 48(sp)
+    ld   s1, 40(sp)
+    ld   s2, 32(sp)
+    ld   s3, 24(sp)
+    ld   s4, 16(sp)
+    addi sp, sp, 64
+    add  a0, x0, x0
+    jalr x0, ra, 0
